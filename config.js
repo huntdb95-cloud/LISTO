ba7953d9b3d4e8cfec49b3cc1d077c97360441bd
huntdb95-cloud <hunt.db.95@gmail.com>
2025-12-20 13:07:01 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,11 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+// config.js (ES module)
+// Initializes Firebase + exports auth/db for use across your site
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
+
+export const firebaseConfig = {
   apiKey: "AIzaSyBr6b0E8GN3svOILHgO2agCkW2VsJQIrdM",
   authDomain: "listo-c6a60.firebaseapp.com",
   projectId: "listo-c6a60",
@@ -8,3 +14,7 @@ const firebaseConfig = {
   appId: "1:646269984812:web:6053f752c0d3c74f653189",
   measurementId: "G-HGZS09TX8G"
 };
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
