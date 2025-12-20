3575f371d437fc260a0f57666f291f2c51a66352
huntdb95-cloud <hunt.db.95@gmail.com>
2025-12-19 20:54:09 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,10 @@
 // config.js
-// Paste your Firebase config object here (from Firebase Console → Project settings → Your apps)
 export const firebaseConfig = {
-  apiKey: "PASTE_ME",
-  authDomain: "PASTE_ME",
-  projectId: "PASTE_ME",
-  storageBucket: "PASTE_ME",
-  messagingSenderId: "PASTE_ME",
-  appId: "PASTE_ME"
+  apiKey: "AIzaSyCAkQUZIqcYu1_m_91cCxTa0prpBhInJRc",
+  authDomain: "listo-10872.firebaseapp.com",
+  projectId: "listo-10872",
+  storageBucket: "listo-10872.firebasestorage.app",
+  messagingSenderId: "310589464806",
+  appId: "1:310589464806:web:fda9fa0b4ad973f1c5db27",
+  measurementId: "G-E9Z6TMHYCS"
 };
-
