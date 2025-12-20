97dee88009463fbdcd85aca5f1ef45891042a6bf
huntdb95-cloud <hunt.db.95@gmail.com>
2025-12-20 12:49:52 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
-// config.js
-export const firebaseConfig = {
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
   apiKey: "AIzaSyBr6b0E8GN3svOILHgO2agCkW2VsJQIrdM",
-    authDomain: "listo-c6a60.firebaseapp.com",
-    projectId: "listo-c6a60",
-    storageBucket: "listo-c6a60.firebasestorage.app",
-    messagingSenderId: "646269984812",
-    appId: "1:646269984812:web:6053f752c0d3c74f653189",
-    measurementId: "G-HGZS09TX8G"
+  authDomain: "listo-c6a60.firebaseapp.com",
+  projectId: "listo-c6a60",
+  storageBucket: "listo-c6a60.firebasestorage.app",
+  messagingSenderId: "646269984812",
+  appId: "1:646269984812:web:6053f752c0d3c74f653189",
+  measurementId: "G-HGZS09TX8G"
 };
