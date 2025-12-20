e91351cfa2c1207cc24d37eafb3cafe3595e8360
huntdb95-cloud <hunt.db.95@gmail.com>
2025-12-20 13:08:43 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,11 @@
 // config.js (ES module)
-// Initializes Firebase + exports auth/db for use across your site
+// Initializes Firebase and exports auth + db for your site
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
 
+// Your existing project settings:
 export const firebaseConfig = {
   apiKey: "AIzaSyBr6b0E8GN3svOILHgO2agCkW2VsJQIrdM",
   authDomain: "listo-c6a60.firebaseapp.com",
