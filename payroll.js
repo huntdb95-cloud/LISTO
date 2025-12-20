151ba0b5e617dc006c5fc2a8a0e15432a9af8a43
huntdb95-cloud <hunt.db.95@gmail.com>
2025-12-19 22:30:11 -0600

    Update payroll.js

--- payroll.js
+++ payroll.js
@@ -22,10 +22,13 @@ import {
 
 // ✅ Paste your existing Firebase config here:
 const firebaseConfig = {
-  // apiKey: "...",
-  // authDomain: "...",
-  // projectId: "...",
-  // etc...
+    apiKey: "AIzaSyBr6b0E8GN3svOILHgO2agCkW2VsJQIrdM",
+    authDomain: "listo-c6a60.firebaseapp.com",
+    projectId: "listo-c6a60",
+    storageBucket: "listo-c6a60.firebasestorage.app",
+    messagingSenderId: "646269984812",
+    appId: "1:646269984812:web:6053f752c0d3c74f653189",
+    measurementId: "G-HGZS09TX8G"
 };
 
 const app = initializeApp(firebaseConfig);
